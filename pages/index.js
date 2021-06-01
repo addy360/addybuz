430730136239e4ccfba93289cbba84d5055d5dff
addy360 <abbyboy360@gmail.com>
2021-06-01 16:25:30 +0300

    brands

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Layout from "../components/Layouts";
 import Banner from "../components/Layouts/Home/Banner";
 import BestSales from "../components/Layouts/Home/BestSales";
+import Brands from "../components/Layouts/Home/Brands";
 import Categories from "../components/Layouts/Home/Categories";
 import NewArivals from "../components/Layouts/Home/NewArivals";
 import Offers from "../components/Layouts/Home/Offers";
@@ -18,6 +19,7 @@ const Home = () => {
       <BestSales sec_title="Best Sellers" />
       <BestSales sec_title="Hot Trends" />
       <Reviews />
+      <Brands />
     </Layout>
   );
 };
