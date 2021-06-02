1284653984b6f51b874efa1affdcf319253a7b39
addy360 <abbyboy360@gmail.com>
2021-06-02 15:54:11 +0300

    contact page

--- pages/_document.js
+++ pages/_document.js
@@ -1,53 +1,60 @@
-import Document, { Head, Main, NextScript, Html } from 'next/document'
+import Document, { Head, Main, NextScript, Html } from "next/document";
 export default class CustomDocument extends Document {
-  static async getInitialProps (ctx) {
-    return await Document.getInitialProps(ctx)
+  static async getInitialProps(ctx) {
+    return await Document.getInitialProps(ctx);
   }
 
-  render () {
+  render() {
     return (
-      <Html >
+      <Html>
         <Head>
-        <link
-          rel="stylesheet"
-          type="text/css"
-          href="/static/styles/bootstrap4/bootstrap.min.css"
-        />
-        <link
-          href="/static/plugins/fontawesome-free-5.0.1/css/fontawesome-all.css"
-          rel="stylesheet"
-          type="text/css"
-        />
-        <link
-          rel="stylesheet"
-          type="text/css"
-          href="/static/plugins/OwlCarousel2-2.2.1/owl.carousel.css"
-        />
-        <link
-          rel="stylesheet"
-          type="text/css"
-          href="/static/plugins/OwlCarousel2-2.2.1/owl.theme.default.css"
-        />
-        <link
-          rel="stylesheet"
-          type="text/css"
-          href="/static/plugins/OwlCarousel2-2.2.1/animate.css"
-        />
-        <link
-          rel="stylesheet"
-          type="text/css"
-          href="/static/plugins/slick-1.8.0/slick.css"
-        />
-        <link rel="stylesheet" type="text/css" href="/static/styles/main_styles.css" />
-        <link rel="stylesheet" type="text/css" href="/static/styles/responsive.css" />
+          <link
+            rel="stylesheet"
+            type="text/css"
+            href="/static/styles/bootstrap4/bootstrap.min.css"
+          />
+          <link
+            href="/static/plugins/fontawesome-free-5.0.1/css/fontawesome-all.css"
+            rel="stylesheet"
+            type="text/css"
+          />
+          <link
+            rel="stylesheet"
+            type="text/css"
+            href="/static/plugins/OwlCarousel2-2.2.1/owl.carousel.css"
+          />
+          <link
+            rel="stylesheet"
+            type="text/css"
+            href="/static/plugins/OwlCarousel2-2.2.1/owl.theme.default.css"
+          />
+          <link
+            rel="stylesheet"
+            type="text/css"
+            href="/static/plugins/OwlCarousel2-2.2.1/animate.css"
+          />
+          <link
+            rel="stylesheet"
+            type="text/css"
+            href="/static/plugins/slick-1.8.0/slick.css"
+          />
+          <link
+            rel="stylesheet"
+            type="text/css"
+            href="/static/styles/main_styles.css"
+          />
+          <link
+            rel="stylesheet"
+            type="text/css"
+            href="/static/styles/responsive.css"
+          />
         </Head>
 
         <body>
+          <Main />
 
-          <Main/>
-          
           <NextScript />
-          
+
           <script src="/static/js/jquery-3.3.1.min.js"></script>
           <script src="/static/styles/bootstrap4/popper.js"></script>
           <script src="/static/styles/bootstrap4/bootstrap.min.js"></script>
@@ -62,6 +69,6 @@ export default class CustomDocument extends Document {
           <script src="/static/js/custom.js"></script>
         </body>
       </Html>
-    )
+    );
   }
 }
